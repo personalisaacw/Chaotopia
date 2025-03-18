270a0c7a0c50c59c05bdd5e00ffe2ec4b8ba76eb
Justin Lewis Rowbotham <jrowbot2@uwo.ca>
2025-03-18 03:12:22 +0000

    The Score class and its JUnit tests.

--- src/test/java/com/example/chaotopia/ScoreTest.java
+++ src/test/java/com/example/chaotopia/ScoreTest.java
@@ -1,4 +1,33 @@
+package com.example.chaotopia;
+
+import com.example.chaotopia.Score;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit test for the Score class.
+ */
 class ScoreTest {
-  
+
+    /**
+     * Test for the getScore method, of the Score class.
+     */
+    @Test
+    public void testGetScore() {
+        int expScore = 0;
+        Score score = new Score(0);
+        assertEquals(expScore, score.getScore());
+    }
+
+    /**
+     * Test for the setScore method, of the Score class.
+     */
+    @Test
+    public void testSetScore() {
+        int expScore = 10;
+        Score score = new Score(0);
+        score.setScore(score.getScore() + 10);
+        assertEquals(expScore, score.getScore());
+    }
 }
