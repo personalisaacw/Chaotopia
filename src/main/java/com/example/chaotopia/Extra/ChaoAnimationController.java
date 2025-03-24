45eeaacb63df26521dab496926ed422774d5a084
Rosaline Scully <rscully5@uwo.ca>
2025-03-23 21:00:25 -0400

    fixed path for HelloApp

--- src/main/java/com/example/chaotopia/Extra/ChaoAnimationController.java
+++ src/main/java/com/example/chaotopia/Extra/ChaoAnimationController.java
@@ -1,8 +1,5 @@
-<<<<<<<< HEAD:src/main/java/com/example/chaotopia/Extra/ChaoAnimationController.java
 package com.example.chaotopia.Extra;
-========
-package com.example.chaotopia.Model;
->>>>>>>> 196c8d0 (added FruitAnimation.java, FruitType.java, FruitAnimation.fxml, and some testing classes FruitAnimationController.java, FruitAnimationTest.java. I also changed the file structure.):src/main/java/com/example/chaotopia/Model/HelloController.java
+
 
 
 import com.example.chaotopia.Model.AnimationState;
