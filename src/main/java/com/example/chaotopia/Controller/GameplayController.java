907867819d61270f442b2f4c2fbedb3b8c8c41b1
Rosaline Scully <rscully5@uwo.ca>
2025-03-26 23:12:35 -0400

    animation controller

--- src/main/java/com/example/chaotopia/Controller/GameplayController.java
+++ src/main/java/com/example/chaotopia/Controller/GameplayController.java
@@ -3,156 +3,350 @@ import com.example.chaotopia.Model.Chao;
 import com.example.chaotopia.Model.Commands;
 import com.example.chaotopia.Model.Inventory;
 import com.example.chaotopia.Model.Item;
+import com.example.chaotopia.Model.Score;
 
-public class GameplayController extends BaseController {
+import com.example.chaotopia.Model.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * GameplayController extends BaseController and integrates with ChaoStatusController
+ * to manage game interaction and UI updates.
+ * @author Issac Wang, Rosaline Scully
+ */
+public class GameplayController extends BaseController implements Initializable {
+
+    //JavaFX
+    @FXML private Label scoreLabel;
+
+    //Instances
     private Chao chao;
     private Inventory inventory;
+    private GameplayAnimationController statusController;
+    private Score score;
 
     //todo: loadGame function (calls load game class)
 
+    /**
+     * Initializes the controller with required resources.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // change everything in here based on load game
+        inventory = new Inventory();
+        statusController = new GameplayAnimationController();
+        score = new Score(0);
+
+        // Add some default items for testing
+        inventory.addItem("Red Fruit", 20);
+        inventory.addItem("Blue Fruit", 20);
+        inventory.addItem("Green Fruit", 20);
+        inventory.addItem("Hero Fruit", 20);
+        inventory.addItem("Dark Fruit", 20);
+        inventory.addItem("Trumpet", 20);
+        inventory.addItem("Duck", 20);
+        inventory.addItem("T.V.", 20);
+    }
+
+    /**
+     * Sets the ChaoStatusController reference to enable UI updates.
+     *
+     * @param statusController The ChaoStatusController instance
+     */
+    public void setStatusController(GameplayAnimationController statusController) {
+        this.statusController = statusController;
+    }
+
+    /**
+     * Sets the Chao reference for both this controller and the status controller.
+     *
+     * @param chao The Chao object
+     */
+    public void setChao(Chao chao) {
+        this.chao = chao;
+
+        // Update the status controller if available
+        if (statusController != null) {
+            statusController.setChao(chao);
+        }
+    }
+
+
 
     //play function
+    @FXML
     public void playChao() {
-        Commands.play(chao);
+        if (isInteractionAllowed()) {
+            Commands.play(chao);
+
+            // Update UI and show animation
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(10);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        }
     }
 
     //sleep function
+    @FXML
     public void sleepChao() {
-        Commands.sleep(chao);
+        if (isInteractionAllowed()) {
+            Commands.sleep(chao);
+
+            // Update UI and manage sleep state
+            if (statusController != null && chao.getState() == State.SLEEPING) {
+                statusController.updateStatusBars();
+                score.updateScore(7);
+                updateScoreUI(score.getScore());
+                statusController.startSleepIncrease();
+                statusController.enableAllInteractions(false); // Disable buttons during sleep
+            }
+        }
         //todo: add keyboard shortcuts
-        //todo: need to update the animation of the chao
+        //todo: play the chao animation
+        //todo: play the proper sound
     }
 
     //exercise function
+    @FXML
     public void exerciseChao() {
-        Commands.exercise(chao);
-        //todo: need to update the animation of the chao
+        if (isInteractionAllowed()) {
+            Commands.exercise(chao);
+
+            // Update UI and show animation
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(10);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        }
+        //todo: add keyboard shortcuts
+        //todo: play the chao animation
+        //todo: play the proper sound
     }
 
     //vet function
+    @FXML
     public void vetChao() {
-        Commands.vet(chao);
-        //todo: need to update the animation of the chao
+        if (isInteractionAllowed()) {
+            Commands.vet(chao);
+
+            // Update UI and possibly deduct points
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(-20);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        }
+        //todo: add keyboard shortcuts
+        //todo: play the chao animation
+        //todo: play the proper sound
     }
 
     //pet function
+    @FXML
     public void petChao() {
-        Commands.pet(chao);
-        //todo: need to update the animation of the chao
+        if (isInteractionAllowed()) {
+            Commands.pet(chao);
+
+            // Update UI and show animation
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(3);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        }
+        //todo: add keyboard shortcuts
+        //todo: play the chao animation
+        //todo: play the proper sound
     }
 
     //bonk function
+    @FXML
     public void bonkChao() {
-        Commands.bonk(chao);
-        //todo: need to update the animation of the chao
+        if (isInteractionAllowed()) {
+            Commands.bonk(chao);
+
+            // Update UI without showing happy animation (it's a bonk!)
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(-3);
+                updateScoreUI(score.getScore());
+
+                // Change to angry state temporarily if not already angry
+                if (chao.getState() != State.ANGRY) {
+                    State previousState = chao.getState();
+                    chao.setState(State.ANGRY);
+
+                    // Force animation update
+                    statusController.updateChaoAnimation(State.ANGRY);
+
+                    // Schedule return to previous state after a delay
+                    statusController.scheduleStateChange(previousState, 2.0); // 2 second delay
+                }
+            }
+        }
+        //todo: add keyboard shortcuts
+        //todo: play the chao animation
+        //todo: play the proper sound
     }
 
     //gifts a trumpet
+    @FXML
     public void giftTrumpet() {
-        //if item exists, give the item and remove it from inventory
-        String itemName = "Trumpet";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.give(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        giftItem("Trumpet");
     }
 
     //gifts a duck
+    @FXML
     public void giftDuck() {
-        //if item exists, give the item and remove it from inventory
-        String itemName = "Duck";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.give(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        giftItem("Duck");
     }
 
     //gifts a tv
+    @FXML
     public void giftTV() {
-        //if item exists, give the item and remove it from inventory
-        String itemName = "T.V.";
-        if (inventory.getItemCount(itemName) > 0) {
+        giftItem("T.V.");
+    }
+
+    /**
+     * Helper method to handle gift items and update UI
+     *
+     * @param itemName The name of the item to gift
+     */
+    private void giftItem(String itemName) {
+        if (isInteractionAllowed() && inventory.getItemCount(itemName) > 0) {
             Commands.give(chao, new Item(itemName));
             inventory.removeItem(itemName);
-            //todo: play sound effect
+
+            // Update UI and show animation
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(10);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        } else {
+            // TODO: Display "No items available" message and or sound effect
+            System.out.println("No " + itemName + " available!");
         }
-        //else:
-        //todo: display "no items available"/play sound effect
     }
 
     public void feedRedFruit() {
-        //if item exists, feed the fruit and remove it from inventory
-        String itemName = "Red Fruit";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.feed(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-            //todo: play the fruit eating animation
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        feedFruit("Red Fruit", false);
     }
 
     public void feedBlueFruit() {
-        //if item exists, feed the fruit and remove it from inventory
-        String itemName = "Blue Fruit";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.feed(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-            //todo: play the fruit eating animation
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        feedFruit("Blue Fruit", false);
     }
 
     public void feedGreenFruit() {
-        //if item exists, feed the fruit and remove it from inventory
-        String itemName = "Green Fruit";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.feed(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-            //todo: play the fruit eating animation
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        feedFruit("Green Fruit", false);
     }
 
     public void feedHeroFruit() {
-        //if item exists, feed the fruit and remove it from inventory
-        String itemName = "Hero Fruit";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.feedSpecialFruit(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-            //todo: play the fruit eating animation
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        feedFruit("Hero Fruit", true);
     }
 
     public void feedDarkFruit() {
-        //if item exists, feed the fruit and remove it from inventory
-        String itemName = "Dark Fruit";
-        if (inventory.getItemCount(itemName) > 0) {
-            Commands.feedSpecialFruit(chao, new Item(itemName));
-            inventory.removeItem(itemName);
-            //todo: play sound effect
-            //todo: play the fruit eating animation
-        }
-        //else:
-        //todo: display "no items available"/play sound effect
+        feedFruit("Dark Fruit", true);
     }
 
-    //todo: save button (saves all data to csv)
+    /**
+     * Helper method to handle feeding fruits and update UI
+     *
+     * @param fruitName The name of the fruit to feed
+     * @param isSpecial Whether this is a special alignment-changing fruit
+     */
+    private void feedFruit(String fruitName, boolean isSpecial) {
+        if (isInteractionAllowed() && inventory.getItemCount(fruitName) > 0) {
+            Item fruit = new Item(fruitName);
 
-    //todo: back to mainMenu button
+            if (isSpecial) {
+                Commands.feedSpecialFruit(chao, fruit);
+
+                // Check if the type changed due to alignment change
+                ChaoType previousType = chao.getType();
+                // Let's assume the Chao updates its type internally based on alignment
+                if (previousType != chao.getType() && statusController != null) {
+                    statusController.updateChaoType(chao.getType());
+                }
+            } else {
+                Commands.feed(chao, fruit);
+            }
+
+            inventory.removeItem(fruitName);
+
+            // Update UI and show animation
+            if (statusController != null) {
+                statusController.updateStatusBars();
+                score.updateScore(5);
+                updateScoreUI(score.getScore());
+                statusController.showHappyAnimation();
+            }
+        } else {
+            // TODO: Display "No items available" message
+            System.out.println("No " + fruitName + " available!");
+        }
+    }
+
+    /**
+     * Saves the current game state.
+     */
+    @FXML
+    public void saveGame() {
+        // TODO: Implement save game functionality to JSON
+        System.out.println("Game saved (not actually implemented yet)");
+    }
+
+    /**
+     * Returns to the main menu.
+     */
+    @FXML
+    public void goToMainMenu() {
+        // TODO: Navigate back to main menu
+        System.out.println("Returning to main menu (not actually implemented yet)");
+    }
+
+    /**
+     * Cleans up resources when the controller is no longer needed.
+     */
+    public void shutdown() {
+        if (statusController != null) {
+            statusController.shutdown();
+        }
+    }
+
+    /**
+     * Checks if interaction with the Chao is allowed based on its current state
+     *
+     * @return true if interaction is allowed, false otherwise
+     */
+    private boolean isInteractionAllowed() {
+        if (chao == null) {
+            return false;
+        }
+
+        // Cannot interact if dead or sleeping (except for specific actions)
+        return !chao.getStatus().isDead() && chao.getState() != State.SLEEPING;
+    }
+
+    /**
+     * Updates the score display.
+     *
+     * @param newScore the new score value
+     */
+    private void updateScoreUI(int newScore) {
+        scoreLabel.setText("SCORE: " + score);
+    }
+
 }
