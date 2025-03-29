f711aca796a588b6afa23b975f7318de95c028fc
Isaac Wang <iwang29@uwo.ca>
2025-03-29 13:03:17 -0500

    added parental controls page UI

--- src/main/java/com/example/chaotopia/Controller/ParentalControlsController.java
+++ src/main/java/com/example/chaotopia/Controller/ParentalControlsController.java
@@ -1,4 +1,60 @@
 package com.example.chaotopia.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
 public class ParentalControlsController extends BaseController{
+
+    @FXML
+    private CheckBox playRangeCheckBox;
+    //todo: go back button should go back to main menu
+
+    @FXML
+    private Label playtimeStats;
+
+    private String totalPlayTime;
+    private String averagePlayTime;
+
+    public void initialize() {
+        //todo: load playtime data
+
+        //todo: format in minutes? hours?
+        playtimeStats.setText("Total Playtime: " + totalPlayTime + "\n" + "Average Playtime: " + averagePlayTime);
+    }
+
+    //saves the playable time range
+    public void submitPlaytimeRange() {
+
+    }
+
+    public void enablePlayRange() {
+        if (playRangeCheckBox.isSelected()) {
+            System.out.println("CheckBox is checked!");
+            // Perform actions when checked
+        } else {
+            System.out.println("CheckBox is unchecked!");
+            // Perform actions when unchecked
+        }
+    }
+
+    public void resetPlaytimeStats() {
+        //todo: reset playtime stats
+    }
+
+    public void reviveSlot1() {
+
+    }
+
+    public void reviveSlot2() {
+
+    }
+
+    public void reviveSlot3() {
+
+    }
+
+    private void handleRevive() {
+
+    }
 }
--- src/main/java/com/example/chaotopia/Controller/ParentalPasswordCheckController.java
+++ src/main/java/com/example/chaotopia/Controller/ParentalPasswordCheckController.java
@@ -1,5 +1,9 @@
 package com.example.chaotopia.Controller;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class ParentalPasswordCheckController extends BaseController {
     private String password;
 
@@ -7,7 +11,10 @@ public class ParentalPasswordCheckController extends BaseController {
         //todo: call parental controls class to set password variable
     }
 
-    public void checkPassword() {
+    public void checkPassword(ActionEvent e) throws IOException {
+        //used for testing
+        switchScene(e, "/com/example/chaotopia/View/ParentalControls.fxml");
+
         //if password correct:
             //todo: call parental controls
             //go to parental controls screen
