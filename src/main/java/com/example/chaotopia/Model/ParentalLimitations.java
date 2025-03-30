3b5a38cfef404180ee726d06634c8d92fc80d040
Justin Lewis Rowbotham <jrowbot2@uwo.ca>
2025-03-30 02:49:25 +0000

    Parental save data

--- src/main/java/com/example/chaotopia/Model/ParentalLimitations.java
+++ src/main/java/com/example/chaotopia/Model/ParentalLimitations.java
@@ -1,5 +1,12 @@
 package com.example.chaotopia.Model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONParserConfiguration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalTime;
 
 /**
@@ -15,12 +22,16 @@ import java.time.LocalTime;
  * @author Justin Rowbotham
  */
 public final class ParentalLimitations {
-    /* Flag determining if the limitations are enabled or not. */
-    static boolean enabled;
-    /* The starting time of play allowed by the parent (in system time). */
-    static java.time.LocalTime allowedStartTime;
-    /* The ending time of play allowed by the parent (in system time). */
-    static java.time.LocalTime allowedEndTime;
+    /** Flag determining if the limitations are enabled or not. */
+    private static boolean enabled;
+    /** The starting time of play allowed by the parent (in system time). */
+    private static java.time.LocalTime allowedStartTime;
+    /** The ending time of play allowed by the parent (in system time). */
+    private static java.time.LocalTime allowedEndTime;
+    /** Path for the limitations save file. */
+    private static final String LIMITATIONS_FILE = "src/main/resources" +
+            "/com/example/chaotopia/Saves/parental_limitations.json";
+
 
     /**
      * Constructor method for parental limitations.
@@ -32,9 +43,22 @@ public final class ParentalLimitations {
      * Method that loads the field variables stored in the save file.
      */
     public static void loadParentalLimitations() {
-        enabled = GameFile.getLimitationStatus();
-        allowedStartTime = GameFile.getLimitationStart();
-        allowedEndTime = GameFile.getLimitationEnd();
+        try {
+            /* Get the .json file. */
+            String content = new String(Files.readAllBytes(Paths
+                    .get(LIMITATIONS_FILE)));
+            JSONObject jsonData = new JSONObject(content);
+
+            /* Load the values from the .json file. */
+            enabled = jsonData.getBoolean("enabled");
+            allowedStartTime = LocalTime.parse(jsonData.getString
+                    ("allowedStartTime"));
+            allowedEndTime = LocalTime.parse(jsonData.getString
+                    ("allowedEndTime"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -67,5 +91,26 @@ public final class ParentalLimitations {
     public static boolean isPlayAllowed(java.time.LocalTime currentTime) {
         return !enabled || (currentTime.isAfter(allowedStartTime)
                 && currentTime.isBefore(allowedEndTime));
+    }
+
+    /**
+     * Method that stores the field variables stored into the save file.
+     * Must be called before exiting the application.
+     */
+    public static void storeParentalLimitations() {
+        try {
+            /* Get the .json file. */
+            String content = new String(Files.readAllBytes(Paths
+                    .get(LIMITATIONS_FILE)));
+            JSONObject jsonData = new JSONObject(content);
+
+            /* Load the values from the .json file. */
+            jsonData.put("enabled", enabled);
+            jsonData.put("allowedStartTime", allowedStartTime);
+            jsonData.put("allowedEndTime", allowedEndTime);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
